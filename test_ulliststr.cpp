a586cafb91ea6d6e4081e292c9e3eca9a3931f85
Angela Zhuang <zhuanga@usc.edu>
2025-01-30 05:51:17 +0000

    all done with part 2

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -5,9 +5,22 @@
 
 #include "ulliststr.h"
 
+using namespace std;
+
 //Use this file to test your ulliststr implementation before running the test suite
 
 int main(int argc, char* argv[])
 {
-
+  ULListStr dat;
+  dat.push_front(to_string(1));
+  dat.push_back(to_string(7));
+  dat.push_front(to_string(8));
+  dat.push_back(to_string(9));
+  dat.pop_back();
+  dat.pop_front();
+  cout << dat.get(0) << " " << dat.get(1) << endl;
+  // prints: 8 7 9
+  cout << dat.size() << endl;  // prints 3 since there are 3 strings stored
+  cout << "Front is: " << dat.front() << endl;
+  cout << "Back is: " << dat.back() << endl;
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -63,3 +63,146 @@ void ULListStr::clear()
   tail_ = NULL;
   size_ = 0;
 }
+
+//Code starts HERE
+std::string* ULListStr::getValAtLoc(size_t loc) const{
+  Item *temp = head_;
+  while(temp != NULL){
+    for(size_t i=temp->first; i<temp->last; i++){
+      if(loc==0){
+        return (temp->val)+i;
+      }
+      loc--;
+    }
+    temp = temp->next;
+  }
+  // Invalid "loc"
+  return NULL;
+}
+
+void ULListStr::push_back(const std::string& val){
+  //If list is empty
+  if(tail_==NULL){
+    head_ = new Item();
+    tail_ = head_;
+    tail_->val[0] = val;
+    tail_->last = 1;
+    tail_->prev = NULL;
+    tail_->next = NULL;
+  } else {
+    //If list is not empty
+    //If the last Item's array is full
+    if(tail_->last==10){
+      tail_->next = new Item(); 
+      (tail_->next)->prev = tail_;
+      tail_ = tail_->next;
+      tail_->next = NULL;
+      tail_->val[0] = val;
+      tail_->last = 1;
+    } else {
+      //If the last Item's array is not full
+      tail_->val[tail_->last] = val;
+      (tail_->last)++;
+    }
+  }
+
+  //Increase size_ by 1
+  size_++;
+}
+
+void ULListStr::pop_back(){
+  //If this is the only value in the List
+  if(size_==1){
+    Item* temp = tail_;
+    tail_ = NULL;
+    head_ = NULL;
+    delete temp;
+  } else {
+    //If this is not the only value in the List
+    //If the Item will be empty after popping
+    if(tail_->last==1){
+      Item* temp = tail_;
+      tail_ = tail_->prev;
+      if(tail_==NULL){
+        //Do nothing
+      } else{
+        tail_->next = NULL;
+        delete temp;
+      }
+    } else {
+      //If the Item will not be empty after popping
+      (tail_->last)--;
+    }
+  }
+
+  //Decrease size by 1
+  size_--;
+}
+
+void ULListStr::push_front(const std::string& val){
+  //If list is empty
+  if(tail_==NULL){
+    head_ = new Item();
+    tail_ = head_;
+    tail_->val[9] = val;
+    tail_->first = 9;
+    tail_->last = 10;
+    tail_->prev = NULL;
+    tail_->next = NULL;
+  } else {
+    //If list is not empty
+    //If the first Item's array is full
+    if(head_->first==0){
+      head_->prev = new Item(); 
+      (head_->prev)->next = head_;
+      head_ = head_->prev;
+      head_->prev = NULL;
+      head_->val[0] = val;
+      head_->last = 1;
+    } else {
+      //If the first Item's array is not full
+      head_->val[(head_->first)-1] = val;
+      (head_->first)--;
+    }
+  }
+
+  //Increase size_ by 1
+  size_++;
+}
+
+void ULListStr::pop_front(){
+  //If this is the only value in the List
+  if(size_==1){
+    Item* temp = tail_;
+    tail_ = NULL;
+    head_ = NULL;
+    delete temp;
+  } else {
+    //If this is not the only value in the List
+    //If the Item will be empty after popping
+    if(head_->first==9){
+      Item* temp = head_;
+      head_ = head_->next;
+      if(head_==NULL){
+        //Do nothing
+      } else {
+        head_->prev = NULL;
+        delete temp;
+      }
+    } else {
+      //If the Item will not be empty after popping
+      (head_->first)++;
+    }
+  }
+
+  //Decrease size by 1
+  size_--;
+}
+
+std::string const & ULListStr::back() const{
+  return tail_->val[(tail_->last)-1];
+}
+
+std::string const & ULListStr::front() const{
+  return head_->val[head_->first];
+}
